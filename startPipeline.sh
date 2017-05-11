1ffca14665a7f8a79231d243ebdcd1c5fc2a2431
FunThomas424242 <funthomas424242@gmail.com>
2017-05-11 22:19:51 +0200

    start script fixed

--- startPipeline.sh
+++ startPipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 docker pull funthomas424242-docker-funthomas424242-container.bintray.io/funthomas424242/jenkins-pipeline
-docker run -e "JP_PROJECT_NAME=slidecast-angularjs.example" -e "JP_SCM_URL=https://github.com/FunThomas424242/slidecast-angularjs.example.git" -p 8080:8080 -p 50000:50000 funthomas424242/jenkins-pipeline 
+docker run -e "JP_PROJECT_NAME=slidecast-angular.example" -e "JP_SCM_URL=https://github.com/FunThomas424242/slidecast-angularjs.example.git" -p 8080:8080 -p 50000:50000 funthomas424242-docker-funthomas424242-container.bintray.io/funthomas424242/jenkins-pipeline
