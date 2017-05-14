5855acf52ba3fb5f45f1476ea590460581fc9f2c
FunThomas424242 <funthomas424242@gmail.com>
2017-05-14 21:00:29 +0200

    Revert "test"
    This reverts commit 6bff3f5981f012534190f7d0509a41c71ed02644.

--- startPipeline.sh
+++ startPipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 docker pull funthomas424242-docker-funthomas424242-container.bintray.io/funthomas424242/jenkins-pipeline
-docker run -e "JP_PROJECT_NAME=slidecast-angular.example" -e "JP_SCM_URL=https://github.com/FunThomas424242/slidecast-angularjs.example.git" -p 8080:8080 -p 50000:50000 -v /home/huluvu424242/git:/var/jenkins_home/workspace funthomas424242/jenkins-pipeline
+docker run -e "JP_PROJECT_NAME=slidecast-angular.example" -e "JP_SCM_URL=https://github.com/FunThomas424242/slidecast-angularjs.example.git" -p 8080:8080 -p 50000:50000 funthomas424242-docker-funthomas424242-container.bintray.io/funthomas424242/jenkins-pipeline
